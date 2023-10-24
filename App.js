b71319b057effe556ae762e0363e8686b782be0a
Joe Rafalak <jrafalak@quickbase.com>
2023-10-24 19:32:18 -0400

    fetch initial data

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { fetchData } from "./utils";
+import { baseUrl, questionsUrlParam, answerUrlParam } from "./consts.json";
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const [answer, setAnswer] = useState(null);
+
+  useEffect(() => {
+    const fetchQuestionDataFromAPI = async () => {
+      const questionsUrl = `${baseUrl}${questionsUrlParam}`;
+      const response = await fetchData(questionsUrl);
+      setData(response);
+    };
+    fetchQuestionDataFromAPI();
+
+    const fetchAnswerDataFromAPI = async () => {
+      const answersUrl = `${baseUrl}${answerUrlParam}${data.id}`;
+      const response = await fetchData(answersUrl);
+      setAnswer(response);
+    };
+    fetchAnswerDataFromAPI();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>testing</Text>
+      <Text>{data ? JSON.stringify(data) : "Loading..."}</Text>
+      <Text>{answer ? JSON.stringify(answer) : "Loading..."}</Text>
       <StatusBar style="auto" />
     </View>
   );
