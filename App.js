033a2b57a5e6df1bfd1ec707bf4762aed414e726
Joe Rafalak <jrafalak@quickbase.com>
2023-10-27 13:05:56 -0400

    add components so far

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { fetchData } from "./utils";
 import { baseUrl, questionsUrlParam, answerUrlParam } from "./consts.json";
 import Header from "./containers/Header/Header";
 import Question from "./components/Question/Question";
+import MultipleChoicePane from "./containers/MultipleChoicePane/MultipleChoicePane.js";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function App() {
   const [data, setData] = useState(null);
   const [answer, setAnswer] = useState(null);
-  const [counter, setCounter] = useState(600);
+  const [counter, setCounter] = useState(600000);
   const [image, setImage] = useState(null);
+  const [options, setOptions] = useState([]);
 
   useEffect(() => {
     const fetchQuestionDataFromAPI = async () => {
@@ -19,9 +28,10 @@ export default function App() {
       const answersUrl = `${baseUrl}${answerUrlParam}${response.id}`;
       setData(response);
       setImage(response.image);
+      setOptions(response.options);
 
       const answerResponse = await fetchData(answersUrl);
-      setAnswer(answerResponse);
+      setAnswer(answerResponse.correct_options[0].id);
     };
     fetchQuestionDataFromAPI();
   }, []);
@@ -31,15 +41,14 @@ export default function App() {
   }, [counter]);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.safeAreaView}>
       <ImageBackground
         source={{ uri: image }}
         resizeMode="cover"
         style={styles.image}
       >
-        <View style={styles.overlay} />
-        <Header />
-
+        {/* <View style={styles.overlay} /> */}
+        <Header counter={counter} />
         <Question text={data ? data.question : "Loading..."} />
 
         <Text style={styles.whiteText}>
@@ -48,34 +57,43 @@ export default function App() {
         <Text style={styles.whiteText}>
           {answer ? JSON.stringify(answer) : "Loading..."}
         </Text>
+        <MultipleChoicePane options={options || []} correctAnswerId={answer} />
+        <NavBar style={styles.navBar} />
         <StatusBar style="auto" />
       </ImageBackground>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    height: "100%",
+  },
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    flexDirection: "column",
-    width: "100%",
   },
   overlay: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
     backgroundColor: "rgba(0,0,0,0.5)",
   },
   image: {
     flex: 1,
+    alignItems: "flex-start",
     justifyContent: "space-evenly",
-    padding: 0,
-    margin: 0,
+    flexDirection: "column",
   },
   whiteText: {
     color: "white",
     textShadowColor: "rgba(0, 0, 0, 0.50)",
     textShadowOffset: { width: -5, height: 5 },
     textShadowRadius: 10,
+  },
+  navBar: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: "100%",
   },
 });
